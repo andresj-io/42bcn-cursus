92e761f914643e8fd77054e088da36c19723005e
Andres Jacome <andresj@Andress-MacBook-Pro.local>
2023-08-05 19:52:38 -0300

    testing 1

--- 1_1_get_next_line/test.c
+++ 1_1_get_next_line/test.c
@@ -6,44 +6,36 @@
 /*   By: andresj <andresj@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 13:00:58 by ajacome-          #+#    #+#             */
-/*   Updated: 2023/08/05 16:21:49 by andresj          ###   ########.fr       */
+/*   Updated: 2023/08/05 19:39:27 by andresj          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include "test.h"
 
+static void	iterate_file(int fd);
+
 int	main(void)
 {
-	test_file("tests/test_file1");
-	test_file("tests/test_file2");
+	printf("%sStarting tests%s\n", BLUE, RESET);
+	printf(" BUFFER_SIZE = %s%i%s", YELLOW, BUFFER_SIZE, RESET);
+	test_file("tests/test_basic");
+	printf("\n");
+	test_file("tests/test_empty");
+	printf("\n");
+	test_file("tests/test_nl");
+	printf("\n");
+	test_file("tests/test_20nl");
+	printf("\n");
 	test_stdin();
 	test_stdout();
 	test_stderr();
 }
 
-void	test_stdin(void)
-{
-	return ;
-}
-
-void	test_stdout(void)
-{
-	return ;
-}
-
-void	test_stderr(void)
-{
-	return ;
-}
-
 void	test_file(const char *path)
 {
 	int		fd;
-	int		line_q;
-	char	*line;
 
-	line_q = 0;
 	printf("\n%s%s%s\n", GREEN, path, RESET);
 	fd = open(path, O_RDONLY);
 	if (fd == -1)
@@ -52,14 +44,29 @@ void	test_file(const char *path)
 		write(1, "File not found\n", 15);
 		return ;
 	}
-	while (++line_q <= 7)
+	iterate_file(fd);
+	close (fd);
+}
+
+static void	iterate_file(int fd)
+{
+	int		line_q;
+	int		flag;
+	char	*line;
+
+	line_q = 0;
+	flag = true;
+	while (flag || line)
 	{
+		flag = false;
 		line = get_next_line(fd);
 		if (!line)
-			printf("\t%sline %i:\t%s%s\n", YELLOW, line_q, RESET, "No line!");
+		{
+			printf("\t%sline %03i: %s%s\n", YELLOW, line_q, RESET, "No line!");
+			break ;
+		}
 		else
-			printf("\t%sline %i:\t%s%s", YELLOW, line_q, RESET, line);
+			printf("\t%sline %03i: %s%s", YELLOW, line_q, RESET, line);
+		line_q++;
 	}
-	close (fd);
-	printf("\n");
 }
--- 1_1_get_next_line/test_std.c
+++ 1_1_get_next_line/test_std.c
@@ -1,29 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.h                                             :+:      :+:    :+:   */
+/*   test_std.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: andresj <andresj@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/02 11:33:01 by ajacome-          #+#    #+#             */
-/*   Updated: 2023/08/05 19:13:53 by andresj          ###   ########.fr       */
+/*   Created: 2023/08/05 19:31:47 by andresj           #+#    #+#             */
+/*   Updated: 2023/08/05 19:32:14 by andresj          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef TEST_H
-# define TEST_H
+#include "get_next_line.h"
+#include "test.h"
 
-# define RED "\033[0;31m"
-# define GREEN "\033[0;32m"
-# define YELLOW "\033[0;33m"
-# define BLUE "\033[0;34m"
-# define PURPLE "\033[0;35m"
-# define RESET "\033[0m"
-# include <stdio.h>
+void	test_stdin(void)
+{
+	return ;
+}
 
-void	test_stdin(void);
-void	test_file(const char *path);
-void	test_stdout(void);
-void	test_stderr(void);
+void	test_stdout(void)
+{
+	return ;
+}
 
-#endif
+void	test_stderr(void)
+{
+	return ;
+}
